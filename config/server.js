eab6b5fab08b73c8b3b0cb8f029b521db9092c73
salahuddeen <salahzain139@gmail.com>
2021-12-25 18:56:03 +0530

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a8b1e56e0b1416119f32e2be298bccab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b1592870-2399-4558-81a4-e06829081e15'
+};
